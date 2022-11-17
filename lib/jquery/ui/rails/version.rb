2138c1de02058cd1df0b648bbdd420ca578f8d85
William Valencia <william.m.valencia@nasa.gov>
2022-11-17 18:15:17 -0500

    Updating jquery-ui to versuib 1.13.2

--- lib/jquery/ui/rails/version.rb
+++ lib/jquery/ui/rails/version.rb
@@ -1,8 +1,8 @@
 module Jquery
   module Ui
     module Rails
-      VERSION = "6.0.1"
-      JQUERY_UI_VERSION = "1.12.1"
+      VERSION = "6.0.3"
+      JQUERY_UI_VERSION = "1.13.2"
     end
   end
 end
